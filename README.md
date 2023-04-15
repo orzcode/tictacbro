100f5ae60908a83a96e56c5b1ea1c8bd36e8a386
orzcode <71000311+orzcode@users.noreply.github.com>
2023-04-15 15:13:50 +1200

    ~

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # tictacbro
+https://www.digitalocean.com/community/tutorials/understanding-prototypes-and-inheritance-in-javascript
+
 
 You’re going to store the gameboard as an array inside of a Gameboard object, so start there! Your players are also going to be stored in objects, and you’re probably going to want an object to control the flow of the game itself.
 
--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-let gameBoard
+let GameBoard = (() => {
+	let tiles = new Array(9).fill(null);
+	return {tiles};
+})();
+console.log(GameBoard.tiles);
+//IIFE-wrapped, AKA a module
+
+
+
+let Player = (name, symbol) => {
+	let outcome = (outcome) => console.log(name + " " + outcome);
+	return { name, symbol, outcome };
+}
+//Factory function, since it 'returns' the needed
+const Orz = Player('orz', 'X');
+Orz.outcome('win');
